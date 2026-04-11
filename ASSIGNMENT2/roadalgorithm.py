eab043dcdae1ae5c28d14284c90713d96ffb35f3
Olawale <taofeekolawale159@gmail.com>
2026-04-11 19:39:16 +0100

    Update Task

---
+++ ASSIGNMENT2/roadalgorithm.py
@@ -0,0 +1,15 @@
+"""Algorithm: Crossing Road Safely
+Start
+Go to the edge of the road
+Look left, right, and left again for oncoming traffic
+If vehicles are coming, then go to step 5
+Else go to step 7
+Wait at the roadside
+Go back to step 3 (keep checking traffic)
+When the road is clear, walk across the road carefully
+Reach the other side of the road
+
+
+Loop step: Steps 3 to 6 (repeatedly checking traffic until it is safe)
+
+Conditional (decision): Step 4 (checking if vehicles are coming)
