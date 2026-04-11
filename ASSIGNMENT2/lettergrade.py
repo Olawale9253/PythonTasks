3b26703510e05b3ba496ccb1293ede78dcf535e5
Olawale <taofeekolawale159@gmail.com>
2026-04-11 17:58:55 +0100

    Almost done

---
+++ ASSIGNMENT2/lettergrade.py
@@ -0,0 +1,25 @@
+grade = int(input("Enter a grade : " ))
+
+
+if grade >= 90  :
+
+    print("Grade A")
+
+if grade >= 80  :
+
+    print("Grade B")
+
+if grade >= 70  :
+
+    print("Grade C")
+
+if grade >= 60   :
+
+    print("Grade D")
+
+if grade <= 60  :
+
+    print ("Grade F")
+
+else:
+    print("Incorrect grade")
