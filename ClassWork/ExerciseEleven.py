5ec01264c12db376327025d153d5f555beadcad3
Olawale <taofeekolawale159@gmail.com>
2026-04-11 15:24:47 +0100

    Task Updates

--- ClassWork/ExerciseEleven.py
+++ ClassWork/ExerciseEleven.py
@@ -2,8 +2,8 @@
 first_digit = int(input("Enter first digit "))
 second_digit = int(input("Enter second digit "))
 third_digit = int(input("Enter third digit "))
-forth_digit = int(input("Enter forth digit "))
+fourth_digit = int(input("Enter fourth digit "))
 fifth_digit = int(input("Enter last digit "))
 
 
-print(first_digit, '   ', second_digit, '    ', third_digit, '    ', forth_digit, '    ', fifth_digit, '    ')
+print(first_digit, '   ', second_digit, '    ', third_digit, '    ', fourth_digit, '    ', fifth_digit, '    ')
--- ClassWork/ExerciseThree.py
+++ ClassWork/ExerciseThree.py
@@ -4,4 +4,4 @@ if (grade >= 90) :
 
     print("Congratulations")
 else:
-    print("You are a failure you supposed to be cutting grass!!!")
+    print("You failed try again next time")
