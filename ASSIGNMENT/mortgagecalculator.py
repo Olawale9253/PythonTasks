7439a4619d2d33ee018909e8e3d371ffe60b14d5
Olawale <taofeekolawale159@gmail.com>
2026-04-13 09:47:43 +0100

    Update Task

---
+++ ASSIGNMENT/mortgagecalculator.py
@@ -0,0 +1,9 @@
+amount = int(input("Enter amount you want to borrow"))
+
+
+p = 100000      # I borrow from semicolon $100,000
+r = 0.005       # Monthly interest rate (0.5%)
+n = 180         # 180 months (15 years)
+
+multiply = 1.005 ** 180  
+
