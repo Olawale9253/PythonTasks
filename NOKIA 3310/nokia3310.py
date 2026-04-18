e3215ef45752dfb17eb421bb6701dfde016279d9
Olawale <taofeekolawale159@gmail.com>
2026-04-18 17:46:34 +0100

    Task Update

--- NOKIA 3310/nokia3310.py
+++ NOKIA 3310/nokia3310.py
@@ -24,3 +24,22 @@ print ("13.SIM services");
 print ("0. Exit");
 print ("Enter choice: ");
 
+user_menu_choice = int(input())
+
+match user_menu_choice:
+    case 1 :
+        print("\t ==== PHONE BOOK ☎️ ==== \n")
+        print("1. Search")
+        print("2. Service Nos.")
+        print("3. Add name")
+        print("4. Erase")
+        print("5. Edit")
+        print("6. Assign tone")
+        print("7. Send b'card")
+        print("8. Options")
+        print("9. Speed dials")
+        print("10.Voice tags")
+        print("Enter choice: ")
+
+sub_menu_choice = int(input())
+        
