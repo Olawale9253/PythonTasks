597f209d955b46d9e6e9af325239a79f159e0280
Olawale <taofeekolawale159@gmail.com>
2026-04-18 17:26:35 +0100

    Task Update

--- NOKIA 3310/nokia3310.py
+++ NOKIA 3310/nokia3310.py
@@ -1,1 +1,26 @@
-print ('\t Welcome To Nokia 3310')
+power_on = True
+
+user_menu_choice = 0
+
+sub_menu_choice = 0
+
+#while power_on != True:
+
+print ('\t Welcome To Nokia 3310 🤝 ')
+
+print ("1. Phone book");
+print ("2. Messages");
+print ("3. Chat");
+print ("4. Call register");
+print ("5. Tones");
+print ("6. Settings");
+print ("7. Call divert");
+print ("8. Games");
+print ("9. Calculator");
+print ("10.Reminders");
+print ("11.Clock");
+print ("12.Profiles");
+print ("13.SIM services");
+print ("0. Exit");
+print ("Enter choice: ");
+
