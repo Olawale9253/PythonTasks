ddd73ca00f0159cd8e85da09b66bd6376a9d04ba
Olawale <taofeekolawale159@gmail.com>
2026-04-20 15:08:23 +0100

    Class Task

---
+++ CLASSWORK/multiplication.py
@@ -0,0 +1,13 @@
+"""prompt user to enter a number, by using a for loop the number should display multiplication table
+
+"""
+
+
+
+num = int(input("Enter a number: "))
+
+
+for number in range(1, 11):
+
+    print((num * number))
+
---
+++ CLASSWORK/pattern.py
@@ -0,0 +1,14 @@
+for number in range(5, 0, -1):
+    for count in range(number, 0, -1):
+        print(count, end = " ")
+
+    print()
+   
+
+#number1 = int(input("Enter number"))
+#
+#for number in range(number1, 0, -1):
+#    for count in range(number, 0, -1):
+#     print(count, end = "")
+#
+#    print()
